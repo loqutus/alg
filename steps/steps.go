a4b2b5a325619e3c4896cdd29b705bb2f10e1fd8
Ruslan Gustomiasov <rusik@4ege.ru>
2020-12-19 15:41:20 +0000

    fix stairs

--- steps/steps.go
+++ steps/steps.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func steps(n int) {
 	s := "#"
 	for i := 0; i < n; i++ {
+		s += " "
+	}
+	fmt.Println(s)
+	for i := 1; i < n; i++ {
+		x := []byte(s)
+		x[i] = byte('#')
+		s = string(x)
 		fmt.Println(s)
-		s += "#"
 	}
 	return
 }
